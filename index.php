c084822a43f16e85cfb6e0c7a6036b8a9419ae4a
Kamau Wanyee <steekam@users.noreply.github.com>
2018-03-16 02:29:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
         else{
             $mail = new PHPMailer;
             $mail->isSMTP();                            // Set mailer to use SMTP
-            $mail->Host = 'smtp.gmail.com';             // Specify main and backup SMTP servers
+            $mail->Host = 'smtp.gmail.com';             // Specify main and backup SMTP servers if using gmail
             $mail->SMTPAuth = true;                     // Enable SMTP authentication
-            $mail->Username = 'stepwany8@gmail.com';   // SMTP username
-            $mail->Password = 'REStaTiOnsCuSeB';        // SMTP password
+            $mail->Username = 'example@gmail.com';       // SMTP username
+            $mail->Password = 'sdjksjncuSeB';           // SMTP password
             $mail->SMTPSecure = 'tls';                  // Enable TLS encryption, `ssl` also accepted
             $mail->Port = 587;                          // TCP port to connect to
 
